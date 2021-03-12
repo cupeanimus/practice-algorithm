bf0a3221dadbf27c6907b06b3d121ed2f5f31a34
kyle <cupeanimus@naver.com>
2021-03-12 21:13:05 +0900

    프로그래머스의 힙 알고리즘 풀이 - 더 맵게 넣은순간 자동으로 정렬되어 밑에서부터 꺼낼 수 있는 PriorityQueue를 사용한다.

--- src/main/java/com/kyle/practicealgorithm/programmers/MoreSpicy.java
+++ src/main/java/com/kyle/practicealgorithm/programmers/MoreSpicy.java
@@ -1,5 +1,7 @@
 package com.kyle.practicealgorithm.programmers;
 
+import java.util.PriorityQueue;
+
 public class MoreSpicy {
 //    더 맵게
 //
@@ -30,18 +32,52 @@ public class MoreSpicy {
 //    모든 음식의 스코빌 지수가 7 이상이 되었고 이때 섞은 횟수는 2회입니다.
 
 
-    //-1을 리턴하는 경우가 있나? 조건을 추가해야할 필요가 있을까??
+    //-1을 리턴하는 경우가 있나? 조건을 추가해야할 필요가 있을까?
 
-    class Solution {
-        public int solution(int[] scoville, int K) {
-            int answer = 0;
+//     [ 힙 정렬이란? ]
+//
+//    이진 트리에 기반한 힙 트리구조를 만들어가며 배열을 정렬시킴
+//    병합정렬의 메모리 효율성 문제를 해결하면서 O(n*logN)의 시간 복잡도를 보장
+//    결국 시간복잡도가 log지수의 증가를 가지려면 소그룹으로 분리시켜 정렬해나가야 함
+//    소그룹으로 쪼개서 정렬하는 방법에 힙 트리(이진 트리) 구조가 적용된 방식이라 볼 수 있음
+//
+//   ​  [ 이진 트리란? ]
+//
+//    하나의 노드에 자식 노드가 2개 이하인 트리 구조
+//​
+//     [ 힙 트리란? ]
+//
+//    이진 트리 구조에서, 노드 간의 관계에 규칙성이 적용 된 구조
+//    최대 힙 구조 : 부모 노드가 자식 노드보다 값이 큼 (가장 위 root 값이 최대값이 됨)
+//    최소 힙 구조 : 최대 힙 구조와 반대
+//    이진 트리를 최대 힙 또는 최소 힙 구조로 완성시킴 (최대값, 최소값을 빠르게 찾을 수 있음)
+//
 
-            return answer;
+    public static int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int i = 0; i < scoville.length; i++) {
+            queue.offer(scoville[i]);
         }
+
+        while (queue.peek() < K) {
+            if (queue.size() < 2) {
+                return -1;
+            }
+            int lowestScoville = queue.poll();
+            int secondScoville = queue.poll();
+            int newScoville = lowestScoville + (secondScoville * 2);
+            queue.offer(newScoville);
+            answer++;
+        }
+        return answer;
     }
+
 
     public static void main(String[] args) {
 
+        int[] scoville = new int[]{1, 2, 3, 9, 10, 12};
+        System.out.println(solution(scoville, 20));
 
 
     }
