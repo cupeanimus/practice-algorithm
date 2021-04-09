c972cc2b4c9ed2fc430fbed2a792ccc6b51620d1
kyle <cupeanimus@naver.com>
2021-04-09 23:42:54 +0900

    N으로 표현 깊이탐색으로 풀이 추가

--- src/main/java/com/kyle/practicealgorithm/programmers/ExpressionN.java
+++ src/main/java/com/kyle/practicealgorithm/programmers/ExpressionN.java
@@ -7,7 +7,48 @@ import java.util.List;
 
 public class ExpressionN {
 
-    //55, 555 고려하지 못함
+    private static int n;
+    private static int target;
+    private static int answer = 9;
+
+
+
+    private static int solution2(int N, int number) {
+        n = N;
+        target = number;
+        searchMinTarget(0, 0);
+
+        return answer > 8 ? -1 : answer;
+
+    }
+
+    private static void searchMinTarget(int count, int prev) {
+        if (count > 8){
+            answer = -1;
+            return;
+        }
+
+        if (prev == target){
+            answer = Math.min(answer, count);
+            return;
+        }
+
+        int tempN = n;
+        for (int i = 0; i < 8 - count; i++) {
+            int newCount = count + i + 1;
+            searchMinTarget(newCount, prev + tempN);
+            searchMinTarget(newCount, prev - tempN);
+            searchMinTarget(newCount, prev / tempN);
+            searchMinTarget(newCount, prev * tempN);
+
+            tempN = tempN * 10 + n;
+
+        }
+
+    }
+
+
+    //55, 555 고려하지 못함  <- 리스트를 중복없게하기위해 set타입으로 바꿔보자
     public static int solution(int N, int number){
         int answer = 1;
 
@@ -17,13 +58,15 @@ public class ExpressionN {
         List<Integer>  repeatedNum = new ArrayList<>();
         repeatedNum.add(N);
         for (int i = 1; i < 8; i++) {
-            repeatedNum.add((int) (repeatedNum.get (i-1)+Math.pow(10,i)*5));
+            repeatedNum.add((int) (repeatedNum.get (i-1)+Math.pow(10,i)*N));
         }
 
 
 
         while ( answer < 9){
-            if (resultList.contains(number)) return answer;
+            if (resultList.contains(number)) {
+                return answer;
+            }
             int size = resultList.size();
 
             for (int i = 0; i < size; i++) {
@@ -45,9 +88,14 @@ public class ExpressionN {
 
 
 
+
+
     public static void main(String[] args) {
-        int result = solution(5,39);
+//        int result = solution(2,11);
+//        System.out.println(result);
+        int result = solution2(2,11);
         System.out.println(result);
+
     }
 
 
