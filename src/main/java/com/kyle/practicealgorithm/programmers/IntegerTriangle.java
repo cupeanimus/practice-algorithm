95bb68ef3ec852f294247be3b282b7610d35630e
kyle <cupeanimus@naver.com>
2021-04-10 23:04:51 +0900

    다른 풀이 비교

--- src/main/java/com/kyle/practicealgorithm/programmers/IntegerTriangle.java
+++ src/main/java/com/kyle/practicealgorithm/programmers/IntegerTriangle.java
@@ -1,5 +1,7 @@
 package com.kyle.practicealgorithm.programmers;
 
+import java.util.Arrays;
+
 public class IntegerTriangle {
 
 
@@ -10,26 +12,40 @@ public class IntegerTriangle {
         int[][] summedTriangle = new int[maxRow][maxRow];//최대 행만큼 최대 열의 크기이므로 맥스가 될 수 있는 행으로 사이즈를 미리 만든다
 
         summedTriangle[0][0] = triangle[0][0];
-        for (int i = 1; i <maxRow ; i++) {
+        for (int i = 1; i < maxRow; i++) {
             for (int j = 0; j < triangle[i].length; j++) {
-                if (j==0){
-                    summedTriangle[i][j] = triangle[i][j] + summedTriangle[i-1][j];
+                if (j == 0) {
+                    summedTriangle[i][j] = triangle[i][j] + summedTriangle[i - 1][j];
                     continue;
                 }
-                if (j == triangle[i].length-1){
-                    summedTriangle[i][j] = triangle[i][j] + summedTriangle[i-1][j-1];
+                if (j == triangle[i].length - 1) {
+                    summedTriangle[i][j] = triangle[i][j] + summedTriangle[i - 1][j - 1];
                     continue;
                 }
-                summedTriangle[i][j] = triangle[i][j] + Math.max(summedTriangle[i-1][j-1],summedTriangle[i-1][j]);
+                summedTriangle[i][j] = triangle[i][j] + Math.max(summedTriangle[i - 1][j - 1], summedTriangle[i - 1][j]);
             }
         }
 
-        for (int i = 0; i < summedTriangle[maxRow-1].length; i++) {
-            answer = answer<summedTriangle[maxRow-1][i]?summedTriangle[maxRow-1][i] : answer;
+        for (int i = 0; i < summedTriangle[maxRow - 1].length; i++) {
+            answer = answer < summedTriangle[maxRow - 1][i] ? summedTriangle[maxRow - 1][i] : answer;
         }
 
 
         return answer;
+    }
+
+
+    //다른 풀이
+    public static int solution2(int[][] triangle) {
+        for (int i = 1; i < triangle.length; i++) {
+            triangle[i][0] += triangle[i - 1][0];
+            triangle[i][i] += triangle[i - 1][i - 1];
+            for (int j = 1; j < i; j++)
+                triangle[i][j] += Math.max(triangle[i - 1][j - 1], triangle[i - 1][j]);
+        }
+
+        //stream을 통한 최대값
+        return Arrays.stream(triangle[triangle.length - 1]).max().getAsInt();
     }
 
 
